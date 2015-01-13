adc7f33e4ba1f7da38f445c329df7f591fa9ef5e
cnanlmlin <369413205@qq.com>
2015-01-13 16:58:14 +0800

    only change format

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
+LOCAL_MODULE := Native
+
 ALL_SOURCES := $(wildcard $(LOCAL_PATH)/*.h) 
 LOCAL_C_INCLUDES := $(ALL_SOURCES:$(LOCAL_PATH)/%=%)
 ALL_SOURCES := $(wildcard $(LOCAL_PATH)/*.cpp) 
@@ -10,8 +12,6 @@ LOCAL_SRC_FILES += $(ALL_SOURCES:$(LOCAL_PATH)/%=%)
 
 LOCAL_CFLAGS += -Werror -D__android__
 LOCAL_CPPFLAGS += -fno-rtti
-
-LOCAL_MODULE := Native
 LOCAL_LDLIBS := -llog -lGLESv2 -lm -lEGL
 
 include $(BUILD_SHARED_LIBRARY)
